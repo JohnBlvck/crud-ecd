d43c80f914d6c418e07968e21c14314f0b8878c9
JonNTR <Jony_10598@hotmail.com>
2023-12-05 11:51:12 -0600

    Creamos los resources de nuestras clases para CRUD

---
+++ crud-john/app/Filament/Resources/AlumnoResource.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AlumnoResource\Pages;
+use App\Filament\Resources\AlumnoResource\RelationManagers;
+use App\Models\Alumno;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AlumnoResource extends Resource
+{
+    protected static ?string $model = Alumno::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nombre_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('nombre')
+                    ->required()
+                    ->maxLength(191),
+                Forms\Components\TextInput::make('apellido_materno')
+                    ->required()
+                    ->maxLength(191),
+                Forms\Components\TextInput::make('apellido_paterno')
+                    ->required()
+                    ->maxLength(191),
+                Forms\Components\TextInput::make('edad')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('nombre')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('apellido_materno')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('apellido_paterno')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('edad')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAlumnos::route('/'),
+            'create' => Pages\CreateAlumno::route('/create'),
+            'edit' => Pages\EditAlumno::route('/{record}/edit'),
+        ];
+    }    
+}
