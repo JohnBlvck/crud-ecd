8bc85b5b13c5672a508e902bfc119648a207f7f8
JonNTR <Jony_10598@hotmail.com>
2023-12-05 11:48:50 -0600

    Agregamos los fillable y las clases de las tablas relacionadas

--- crud-john/app/Models/Alumno.php
+++ crud-john/app/Models/Alumno.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Alumno extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nombre','apellido_materno','apellido_paterno','edad','nombre_id'];
+    // protected $guarded = [];
+
+    //creamos la funcion con el nombre de la tabla que vamos a relacionar
+    public function materias()
+    {
+        return $this->belongsTo(Materia::class,'nombre_id');
+    }
 }
--- crud-john/app/Models/Materia.php
+++ crud-john/app/Models/Materia.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Materia extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nombre'];
+
+    //creamos una funcion con el nombre de la tabla que vamos a relacionar
+    public function estudiantes()
+    {
+        return $this->hasMany(Alumno::class, 'id'); //hasmany (tiene muchos)
+    }
+
 }
